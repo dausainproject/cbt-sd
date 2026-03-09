f54cc9a982e0e815ded52bdef0786a02573d8e4c
bangdauss <bangdauss@gmail.com>
2026-03-09 08:27:41 +0700

    update

---
+++ app/peserta/selesai/page.tsx
@@ -0,0 +1,9 @@
+export default function SelesaiPage() {
+  return (
+    <div style={{ padding: "40px", textAlign: "center" }}>
+      <h1>Ujian Selesai</h1>
+      <p>Terima kasih telah mengikuti ujian.</p>
+      <p>Silakan menunggu pengumuman dari guru.</p>
+    </div>
+  );
+}
---
+++ app/peserta/ujian/page.tsx
@@ -0,0 +1,8 @@
+export default function UjianPage() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>Halaman Ujian</h1>
+      <p>Halaman ujian sedang disiapkan...</p>
+    </div>
+  );
+}
