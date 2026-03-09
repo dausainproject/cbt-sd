6e85c4705c605c0dfc722b43fe49784970f4ae73
bangdauss <bangdauss@gmail.com>
2026-03-09 08:21:55 +0700

    update

---
+++ app/peserta/dashboard/page.tsx
@@ -0,0 +1,8 @@
+export default function DashboardPeserta() {
+  return (
+    <div style={{padding:"40px"}}>
+      <h1>Dashboard Peserta</h1>
+      <p>Halaman dashboard sedang dibuat...</p>
+    </div>
+  )
+}
